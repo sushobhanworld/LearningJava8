5b5797e4e30d64a6abdfe0a5590a932f7bd069e2
Kumar Sushobhan <sushobhanworld@users.noreply.github.com>
2022-02-01 00:29:36 +0530

    Updated code

---
+++ src/practice1/Test.java
@@ -0,0 +1,9 @@
+package practice1;
+
+public class Test {
+    public static void main(String args[]) {
+        String s1 = "Hello";
+        String s2 = "Hellow";
+        System.out.println(s1 + s2);
+    }
+}
